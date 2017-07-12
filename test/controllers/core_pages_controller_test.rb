dd7f493d0a187bc352fb1bd2ee55915617173457
Steve Kedzie <seal.team.smyhk@gmail.com>
2017-07-11 20:57:19 -0700

    imporve markup and add title selector test

--- test/controllers/core_pages_controller_test.rb
+++ test/controllers/core_pages_controller_test.rb
@@ -1,19 +1,27 @@
 require 'test_helper'
 
 class CorePagesControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @base_title = "NW Brewmeisters"
+  end
+
   test "should get about" do
     get core_pages_about_url
     assert_response :success
+    assert_select "title", "About | #{@base_title}"
   end
 
   test "should get contact" do
     get core_pages_contact_url
     assert_response :success
+    assert_select "title", "Contact | #{@base_title}"
   end
 
   test "should get home" do
     get root_url
     assert_response :success
+    assert_select "title", "Home | #{@base_title}"
   end
 
 end
