d2fc150a9110ebe957691bdac521abcdd65bf461
Steve Kedzie <seal.team.smyhk@gmail.com>
2017-07-11 20:31:00 -0700

    update root path

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   get 'core_pages/contact'
 
-  get 'core_pages/home'
+  root 'core_pages#home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
