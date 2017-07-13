feb540ee9a452562fe3d9d3179d5e37747502db9
Steve Kedzie <seal.team.smyhk@gmail.com>
2017-07-13 14:25:34 -0700

    add initial signup tests

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,12 @@ Rails.application.routes.draw do
 
   root 'core_pages#home'
 
-  get '/about',   to: 'core_pages#about'
+  get  '/about',   to: 'core_pages#about'
 
-  get '/contact', to: 'core_pages#contact'
+  get  '/contact', to: 'core_pages#contact'
 
-  get '/signup',  to: 'users#new'
+  get  '/signup',  to: 'users#new'
+  post '/signup', to: 'users#create'
 
   resources :users
 
