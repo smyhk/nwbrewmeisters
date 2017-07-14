37b05f093c6d386552d50db9c26736f97076daf1
Steve Kedzie <seal.team.smyhk@gmail.com>
2017-07-14 14:12:20 -0700

    add permanent cookies

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,4 +31,9 @@ class User < ApplicationRecord
     update_attribute(:remember_digest, User.digest(remember_token))
   end
 
+  # returns true if the given token matches the digest
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
 end
