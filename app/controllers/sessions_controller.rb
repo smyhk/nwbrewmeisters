3f8cd04fbfa1301ae50240dc7d2188c978c69ecf
Steve Kedzie <seal.team.smyhk@gmail.com>
2017-07-17 15:01:54 -0700

    add friendly forwarding

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,7 @@
 class SessionsController < ApplicationController
 
   def new
+    # debugger
   end
 
   def create
@@ -9,7 +10,7 @@ class SessionsController < ApplicationController
       # log the user in and redirect to the user's show page
       log_in @user
       params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
-      redirect_to @user
+      redirect_back_or @user
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
