8bd18119ad703bc2f5cca4755dd6e3a4d4906807
Steve Kedzie <seal.team.smyhk@gmail.com>
2017-07-18 15:23:31 -0700

    add activation logic

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -1,2 +1,17 @@
 class AccountActivationsController < ApplicationController
+
+  def edit
+    user = User.find_by(email: params[:email])
+    if user && !user.activated? && user.authenticated?(:activation, params[:id])
+      user.update_attribute(:activated,    true)
+      user.update_attribute(:activated_at, Time.zone.now)
+      log_in user
+      flash[:success] = "Account activated. Welcome to NW Brewmeisters!"
+      redirect_to user # this will change to root after blog is implemented
+    else
+      flash[:danger] = "Invalid activation link"
+      redirect_to root_url
+    end
+  end
+
 end
