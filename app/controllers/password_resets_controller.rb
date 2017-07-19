53c7b578c91a6d540d529acdaaf4114cd3e36112
Steve Kedzie <seal.team.smyhk@gmail.com>
2017-07-18 17:07:57 -0700

    add reset email method and update attributes

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,7 +1,24 @@
 class PasswordResetsController < ApplicationController
+
   def new
+    
+  end
+
+  def create
+    @user = User.find_by(email: params[:password_reset][:email].downcase)
+    if @user
+      @user.create_reset_digets
+      @user.send_password_reset_email
+      flash[:info] = "Email sent with password reset instructions"
+      redirect_to root_url
+    else
+      flash.now[:danger] = "Invalid email"
+      render 'new'
+    end
   end
 
   def edit
+
   end
+
 end
