64754e1bdde3279f7639f9344485cdd58b2620d3
Steve Kedzie <seal.team.smyhk@gmail.com>
2017-07-18 17:39:22 -0700

    add helper methods and define view

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,5 +1,8 @@
 class PasswordResetsController < ApplicationController
 
+  before_action :get_user,   only: [:edit, :update]
+  before_action :valid_user, only: [:edit, :update]
+
   def new
 
   end
@@ -20,5 +23,19 @@ class PasswordResetsController < ApplicationController
   def edit
 
   end
+
+  private
+
+    def get_user
+      @user = User.find_by(email: params[:email])
+    end
+
+    # confirms a valid user
+    def valid_user
+      unless (@user && @user.activated? &&
+              @user.authenticated?(:reset, params[:id]))
+        redirect_to root_url
+      end
+    end
 
 end
