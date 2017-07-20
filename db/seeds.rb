9ade896436bbc42156203538d03dae190ff57a9e
Steve Kedzie <seal.team.smyhk@gmail.com>
2017-07-20 11:07:02 -0700

    change first seed user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create!(name:  "Test User",
-             email: "example@railstutorial.org",
+User.create!(name:  "King Smyhk",
+             email: "compunomics@yahoo.com",
              password:              "foobar123",
              password_confirmation: "foobar123",
              admin:     true,
