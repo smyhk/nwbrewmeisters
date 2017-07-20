24c42188fd2957e4c1130e3f880478e993930d15
Steve Kedzie <seal.team.smyhk@gmail.com>
2017-07-20 10:29:51 -0700

    add Article model tests and validations

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,7 +1,34 @@
 require 'test_helper'
 
 class ArticleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = users(:steve)
+    @article = Article.new(title: "Hello", body: "Lorem ipsum", user_id: @user.id)
+  end
+
+  test "shoud be valid" do
+    assert @article.valid?
+  end
+
+  test "user id should be present" do
+    @article.user_id = nil
+    assert_not @article.valid?
+  end
+
+  test "title should be present" do
+    @article.title = "    "
+    assert_not @article.valid?
+  end
+
+  test "title should be no more than 100 characters" do
+    @article.title = 'a' * 101
+    assert_not @article.valid?
+  end
+
+  test "body should be present" do
+    @article.body = "    "
+    assert_not @article.valid?
+  end
+
 end
