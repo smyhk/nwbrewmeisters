9a5369233980b322d3d994377103dcd04b112391
Steve Kedzie <seal.team.smyhk@gmail.com>
2017-07-25 15:04:05 -0700

    finish crud model for recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,7 @@
 class RecipesController < ApplicationController
 
-  before_action :logged_in_user
+  before_action :logged_in_user, only: [:new, :create, :edit, :update, :destroy]
+  before_action :correct_user,   only: [:edit, :update, :destroy]
 
   def show
     @recipe = Recipe.find(params[:id])
@@ -20,10 +21,37 @@ class RecipesController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @recipe.update_attributes(recipe_params)
+      #handle a successful update
+      flash[:success] = "Recipe updated"
+      redirect_to @recipe
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @recipe = Recipe.find(params[:id])
+    if @recipe.user == current_user || current_user.admin?
+      @recipe.destroy
+    end
+    flash[:success] = "Recipe deleted"
+    redirect_to current_user
+  end
+
   private
 
     def recipe_params
       params.require(:recipe).permit(:name, :description)
+    end
+
+    def correct_user
+      @recipe = current_user.recipes.find_by(id: params[:id])
+      redirect_to root_url unless current_user.admin? || @recipe.nil?
     end
 
 end
