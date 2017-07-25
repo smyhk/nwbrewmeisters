5617b8aff88c8669467dfbbe649fe16e24d69f81
Steve Kedzie <seal.team.smyhk@gmail.com>
2017-07-25 10:03:21 -0700

    add logged in user

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,9 @@
 class CommentsController < ApplicationController
 
+  before_action :logged_in_user, only: [:create, :edit, :update, :destroy]
+  # before_action :correct_user,   only: [:edit, :update, :destroy]
+  # before_action :admin_user,     only: [:destroy]
+
   def create
     @comment = @commentable.comments.new comment_params
     @comment.user = current_user
