a6e25e081bb94a1282526902fb81835346b46fee
Steve Kedzie <seal.team.smyhk@gmail.com>
2017-07-25 14:14:13 -0700

    add create action and association

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,10 +1,27 @@
 class RecipesController < ApplicationController
 
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
   def new
     @recipe = Recipe.new
   end
 
-  def show
-    @recipe = Recipe.find(params[:id])
+  def create
+    @recipe = current_user.recipes.build(recipe_params)
+    if @recipe.save
+      flash[:success] = "Recipe saved!"
+      redirect_to @recipe
+    else
+      render 'new'
+    end
   end
+
+  private
+
+    def recipe_params
+      params.require(:recipe).permit(:name, :description)
+    end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
 
   has_many :articles, dependent: :destroy
+  has_many :recipes, dependent: :destroy
 
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save   :downcase_email
