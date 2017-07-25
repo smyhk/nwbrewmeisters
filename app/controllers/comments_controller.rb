041300db501e256184f014b5f729d1d8deb7e577
Steve Kedzie <seal.team.smyhk@gmail.com>
2017-07-25 12:47:22 -0700

    add comment delete functionality

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,7 @@ class CommentsController < ApplicationController
 
   before_action :logged_in_user, only: [:create, :edit, :update, :destroy]
   # before_action :correct_user,   only: [:edit, :update, :destroy]
-  # before_action :admin_user,     only: [:destroy]
+  before_action :admin_user,     only: [:destroy]
 
   def create
     @comment = @commentable.comments.new comment_params
