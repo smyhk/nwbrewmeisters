311226dc73bf2613ca092c5a3170c0c37cfb096c
Steve Kedzie <seal.team.smyhk@gmail.com>
2017-07-24 20:10:03 -0700

    add minimal Recipe model to complete polymorphic associations

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ Rails.application.routes.draw do
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :articles
+  resources :recipes
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170725021713) do
+ActiveRecord::Schema.define(version: 20170725023653) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -29,6 +29,15 @@ ActiveRecord::Schema.define(version: 20170725021713) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "recipes", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_recipes_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
