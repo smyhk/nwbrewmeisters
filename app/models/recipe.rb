06e0a2adfb5fb7dd83d41cb058247443df13bdab
Steve Kedzie <seal.team.smyhk@gmail.com>
2017-07-26 10:05:38 -0700

    add model tests

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,4 +3,8 @@ class Recipe < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable
 
+  validates :user_id, presence: true
+  validates :name, presence: true, length: { minimum: 3, maximum: 100 }
+  validates :description, presence:true, length: { minimum: 3, maximum: 240 }
+
 end
--- test/models/recipe_test.rb
+++ test/models/recipe_test.rb
@@ -1,7 +1,51 @@
 require 'test_helper'
 
 class RecipeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = users(:steve)
+    @recipe = @user.recipes.build(name: "Best Ale",
+                                  description: "The ultimate beer!",
+                                  user_id: @user.id)
+  end
+
+  test "shoud be valid" do
+    assert @recipe.valid?
+  end
+
+  test "user id should be present" do
+    @recipe.user_id = nil
+    assert_not @recipe.valid?
+  end
+
+  test "name should be present" do
+    @recipe.name = "    "
+    assert_not @recipe.valid?
+  end
+
+  test "name should be no more than 100 characters" do
+    @recipe.name = 'a' * 101
+    assert_not @recipe.valid?
+  end
+
+  test "name should be no less than 3 characters" do
+    @recipe.name = 'a' * 2
+    assert_not @recipe.valid?
+  end
+
+  test "description should be present" do
+    @recipe.description = "    "
+    assert_not @recipe.valid?
+  end
+
+  test "description should be no more than 240 characters" do
+    @recipe.description = 'a' * 241
+    assert_not @recipe.valid?
+  end
+
+  test "description should be no less than 3 characters" do
+    @recipe.description = 'a' * 2
+    assert_not @recipe.valid?
+  end
+
 end
