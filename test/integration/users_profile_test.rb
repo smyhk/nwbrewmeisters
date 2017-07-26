f302b3352a274086c4ec6fc5f3cb9261a22af0e6
Steve Kedzie <seal.team.smyhk@gmail.com>
2017-07-26 11:23:30 -0700

    test recipes on user profile

--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -3,21 +3,31 @@ require 'test_helper'
 class UsersProfileTest < ActionDispatch::IntegrationTest
 
   def setup
-   @user = users(:steve)
- end
+    @user = users(:steve)
+  end
 
- test "profile display" do
-   get user_path(@user)
-   assert_template 'users/show'
-   assert_select 'title', full_title(@user.name)
-   assert_select 'h1', text: @user.name
-   assert_select 'h1>img.gravatar'
-   assert_match @user.articles.count.to_s, response.body
-   assert_select 'div.pagination', count: 1
-   @user.articles.paginate(page: 1).each do |article|
-     assert_match article.title, response.body
-     assert_match article.body, response.body
-   end
- end
+  test "profile display articles" do
+    get user_path(@user)
+    assert_template 'users/show'
+    assert_select 'title', full_title(@user.name)
+    assert_select 'h1', text: @user.name
+    assert_select 'h1>img.gravatar'
+    assert_match @user.articles.count.to_s, response.body
+    assert_select 'div.pagination', count: 1
+    @user.articles.paginate(page: 1).each do |article|
+      assert_match article.title, response.body
+      assert_match article.body, response.body
+    end
+  end
+
+  test "profile display recipes" do
+    get user_path(@user)
+    assert_match @user.recipes.count.to_s, response.body
+    assert_select 'div.pagination', count: 1
+    @user.recipes.paginate(page: 1).each do |recipe|
+      assert_match recipe.name, response.body
+      assert_match recipe.description, response.body
+    end
+  end
 
 end
