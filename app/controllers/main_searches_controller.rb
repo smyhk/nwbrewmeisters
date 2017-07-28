430584ce35c0a996a245ccc5cd34a3a69561068e
Steve Kedzie <seal.team.smyhk@gmail.com>
2017-07-28 11:49:53 -0700

    define initial search action

--- app/controllers/main_searches_controller.rb
+++ app/controllers/main_searches_controller.rb
@@ -2,7 +2,7 @@ class MainSearchesController < ApplicationController
 
   def search
     @articles = Article.ransack(params[:p].result(distinct: true))
-    render json: { articles: [], recpes: [] }
+    @recipes  = Recipe.ransack(params[:p].result(distinct: true))
   end
 
 end
