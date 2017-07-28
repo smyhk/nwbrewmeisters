7339859a48232d3771464427c801d1a155f6a0ec
Steve Kedzie <seal.team.smyhk@gmail.com>
2017-07-28 12:10:45 -0700

    force json

--- app/controllers/main_searches_controller.rb
+++ app/controllers/main_searches_controller.rb
@@ -1,8 +1,16 @@
 class MainSearchesController < ApplicationController
 
+  before_action :force_json, only: :search
+
   def search
     @articles = Article.ransack(params[:p]).result(distinct: true)
     @recipes  = Recipe.ransack(params[:p]).result(distinct: true)
   end
+
+  private
+
+    def force_json
+      request.format = :json
+    end
 
 end
