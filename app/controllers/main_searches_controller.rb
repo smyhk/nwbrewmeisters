5bf9cc500696f62b51b3263fbccf410aacc147f7
Steve Kedzie <seal.team.smyhk@gmail.com>
2017-07-28 12:08:41 -0700

    add json builder for articles and recipes

--- app/controllers/main_searches_controller.rb
+++ app/controllers/main_searches_controller.rb
@@ -1,8 +1,8 @@
 class MainSearchesController < ApplicationController
 
   def search
-    @articles = Article.ransack(params[:p].result(distinct: true))
-    @recipes  = Recipe.ransack(params[:p].result(distinct: true))
+    @articles = Article.ransack(params[:p]).result(distinct: true)
+    @recipes  = Recipe.ransack(params[:p]).result(distinct: true)
   end
 
 end
---
+++ app/views/main_searches/search.json.jbuilder
@@ -0,0 +1,13 @@
+json.articles do
+  json.array!(@articles) do |article|
+    json.title article.title
+    json.url article_path(article)
+  end
+end
+
+json.recipes do
+  json.array!(@recipes) do |recipe|
+    json.name recipe.name
+    json.url recipe_path(recipe)
+  end
+end
